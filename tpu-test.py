c5fc8dbd03c5eb86067dcc9690690f1c9d3a2fae
christiantjwilliams <christian.tj.williams@gmail.com>
2022-12-29 12:50:56 -0500

    Update tpu-test.py

--- tpu-test.py
+++ tpu-test.py
@@ -8,49 +8,83 @@ import torchvision.models as models
 
 import sys
 
-class LeNet(torch.nn.Module):
-    def __init__(self):
-        super(LeNet, self).__init__()
-        self.linear1 = torch.nn.Linear(100, 200)
-        self.activation = torch.nn.ReLU()
-        self.linear2 = torch.nn.Linear(200, 10)
-        self.softmax = torch.nn.Softmax()
-
-    def forward(self, x):
-        x = self.linear1(x)
-        x = self.activation(x)
-        x = self.linear2(x)
-        x = self.softmax(x)
-        return x
-
 print('if stalls here, check tpu ip address')
 device = xm.xla_device()
 
-# LeNet
-#model = LeNet()
-#model.to(device)
+model_to_profile = "alexnet"
 
-#layer1 = pickle.load(open("fc_1.p", "rb"))
-#layer2 = pickle.load(open("fc_2.p", "rb"))
-#layer3 = pickle.load(open("fc_3.p", "rb"))
+if model_to_profile == "lenet":
+    class LeNet(torch.nn.Module):
+        def __init__(self):
+            super(LeNet, self).__init__()
+            self.linear1 = torch.nn.Linear(100, 200)
+            self.activation = torch.nn.ReLU()
+            self.linear2 = torch.nn.Linear(200, 10)
+            self.softmax = torch.nn.Softmax()
 
-#print(len(layer1), len(layer1[0]), len(layer2), len(layer2[0]), len(layer3), len(layer3[0]))
+        def forward(self, x):
+            x = self.linear1(x)
+            x = self.activation(x)
+            x = self.linear2(x)
+            x = self.softmax(x)
+            return x
 
-# GPT-2
-from transformers import GPT2Tokenizer, GPT2Model
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-# model = GPT2Model.from_pretrained('gpt2').to(device)
+    # LeNet
+    #model = LeNet()
+    #model.to(device)
 
-import random
-import string
+    #layer1 = pickle.load(open("fc_1.p", "rb"))
+    #layer2 = pickle.load(open("fc_2.p", "rb"))
+    #layer3 = pickle.load(open("fc_3.p", "rb"))
 
-def get_random_string(length):
-    # With combination of lower and upper case
-    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
-    # print random string
-    return result_str
+    #print(len(layer1), len(layer1[0]), len(layer2), len(layer2[0]), len(layer3), len(layer3[0]))
 
-model = models.alexnet(pretrained=True).to(device)
+elif model_to_profile == "GPT":
+    # GPT-2
+    from transformers import GPT2Tokenizer, GPT2Model
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    # model = GPT2Model.from_pretrained('gpt2').to(device)
+
+    import random
+    import string
+
+    def get_random_string(length):
+        # With combination of lower and upper case
+        result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+        # print random string
+        return result_str
+
+elif model_to_profile == "alexnet":
+    model = models.alexnet(pretrained=True).to(device)
+    
+def get_dummy_input(model_type):
+    '''
+    random rgb image for CV input
+    '''
+    if model_type == 'alexnet' or model_type == 'lenet':
+        return torch.randn(1, 3,224,224,dtype=torch.float).to(device)
+    elif model_type == 'GPT':
+        return tokenizer(get_random_string(50), return_tensors='pt').to(device)
+
+def warm_up(model_type):
+    '''
+    warm up GPU
+    '''
+    if model_type == 'alexnet' or model_type == 'lenet':
+        for i in range(100):
+            _ = model(dummy_input)
+    elif model_type == 'GPT':
+        for i in range(100):
+            _ = model(**dummy_input)
+
+def infer(model_type):
+    '''
+    run one inference
+    '''
+    if model_type == 'alexnet' or model_type == 'lenet':
+        _ = model(dummy_input)
+    elif model_type == 'GPT':
+        _ = model(**dummy_input)
 
 # get model size
 param_size = 0
@@ -62,27 +96,18 @@ for buffer in model.buffers():
 size_all_mb = (param_size + buffer_size) / 1024**2
 print('model size: {:.3f}MB'.format(size_all_mb))
 
-dummy_input = torch.randn(1, 3,224,224,dtype=torch.float).to(device)
-# dummy input for GPT-2
-# dummy_input = tokenizer(get_random_string(50), return_tensors='pt').to(device)
+dummy_input = get_dummy_input(model_to_profile)
 repetitions = 300
 timings=np.zeros((repetitions,1))
 #GPU-WARM-UP
-for i in range(100):
-   _ = model(dummy_input)
-   # GPT-2 warm-up
-   # _ = model(**dummy_input)
+warm_up(model_to_profile)
 # MEASURE PERFORMANCE
 with torch.no_grad():
   for rep in range(repetitions):
-     dummy_input = torch.randn(1, 3,224,224,dtype=torch.float).to(device)
-     # dummy_input = tokenizer(get_random_string(50), return_tensors='pt').to(device)
-     #start = time.perfcounter()
+     dummy_input = get_dummy_input(model_to_profile)
      start = time.time()
-     _ = model(dummy_input)
-     #nference_time = time.perfcounter() - start
+     infer(model_to_profile)
      inference_time = time.time() - start
-     #_ = model(**dummy_input)
      # WAIT FOR GPU SYNC
      timings[rep] = inference_time
 mean_syn = np.sum(timings) / repetitions
