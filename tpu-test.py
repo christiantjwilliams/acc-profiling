292f554dfa7071fd1939dd0f48c2954786fe7a63
christiantjwilliams <christian.tj.williams@gmail.com>
2023-01-01 19:52:58 -0500

    Update tpu-test.py

--- tpu-test.py
+++ tpu-test.py
@@ -41,7 +41,7 @@ elif model_to_profile == "GPT":
     # GPT-2
     from transformers import GPT2Tokenizer, GPT2Model
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    # model = GPT2Model.from_pretrained('gpt2').to(device)
+    model = GPT2Model.from_pretrained('gpt2').to(device)
 
     import random
     import string
